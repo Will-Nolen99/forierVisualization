d8516e8d7c226a85b78c1c2e6f45f68d1a062756
William Nolen <wpncomps@yahoo.com>
2020-10-21 22:33:25 -0400

    Base done before adding tracing pictures

--- forierVisualization/src/FourierVisual.java
+++ forierVisualization/src/FourierVisual.java
@@ -18,7 +18,7 @@ public class FourierVisual extends PApplet {
 
     // identical use to setup in Processing IDE except for size()
     public void setup(){
-        background(55);
+        background(black);
         stroke(255);
         strokeWeight(1); 
     }
@@ -40,7 +40,18 @@ public class FourierVisual extends PApplet {
     
     final float radiusConstant = 50;
     float time = 0;
-    final int iterations = 2;
+    final int iterations = 10;
+    
+    //colors
+    final int purple = color(125, 122, 188);
+    final int red = color(222, 60, 75);
+    final int green = color(19, 111, 99);
+    final int blue = color(38, 237, 247);
+    final int black = color(40, 38, 44);
+    
+    
+    
+    
     
     ArrayList<Float> wavePoints = new ArrayList<Float>();
     
@@ -49,9 +60,9 @@ public class FourierVisual extends PApplet {
     public void draw(){
     	
     	//Start with background to clear canvas
-    	background(55);
+    	background(black);
     	
-    	translate(width/5, height/2);
+    	translate(100, height/2);
     	
     	
     	
@@ -70,17 +81,18 @@ public class FourierVisual extends PApplet {
     		
         	noFill();
         	strokeWeight(1);
-        	stroke(255, 100);
+        	stroke(green);
         	
         	circle(prevX, prevY, radius * 2);
         	
         	
         	strokeWeight(3);
-        	stroke(255);
+        	stroke(red);
         	point(x, y);
         	
         	
         	strokeWeight(1);
+        	stroke(red);
         	line(prevX, prevY, x, y);
         	
         	prevX = x;
@@ -89,7 +101,7 @@ public class FourierVisual extends PApplet {
     	}
     	
     	//line from end of last circle to where the wave is being drawn
-    	stroke(255, 100);
+    	stroke(blue, 50);
     	line(prevX, prevY, width/5, prevY);
     	
     	
@@ -99,7 +111,7 @@ public class FourierVisual extends PApplet {
     	
     	wavePoints.add(0, prevY);
     	
-    	stroke(255);
+    	stroke(purple);
     	beginShape();
 		
     	for(int i = 0; i < wavePoints.size(); i++) {
@@ -113,8 +125,8 @@ public class FourierVisual extends PApplet {
 
 		
     	//remove points not on screen to save performance
-		// 1000 is how many points it takes to reach the end of the screen
-    	if(wavePoints.size() > 1000) {
+		// 1100 is how many points it takes to reach the end of the screen
+    	if(wavePoints.size() > 1100) {
     		
     		wavePoints.remove(wavePoints.size() - 1);
     		
@@ -124,6 +136,7 @@ public class FourierVisual extends PApplet {
     	
     	double dt = 0.01;
     	time += dt;
+    	
     	
     }
 }
